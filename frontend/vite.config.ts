5cd8dad8e66508dcb6cf2f540789fa07fbc5311d
aishindev <aishindevwork@gmail.com>
2025-12-09 10:44:19 -0300

    config problem resolved

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,28 +1,6 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from "@vitejs/plugin-react";
 
-/*
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: "192.168.100.112",
-    port: 5173,
-    proxy: {
-      "/api/": {
-        target: "http://192.168.100.112:3000",
-      },
-      "/socket.io": {
-        target: "http://192.168.100.112:3000",
-        changeOrigin: true,
-        secure: true,
-        ws: true,
-      },
-    },
-  },
-});
-*/
-
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
   return {
@@ -32,10 +10,10 @@ export default defineConfig(({ mode }) => {
       port: env.VITE_PORT ? Number(env.VITE_PORT) : 5173,
       proxy: {
         "/api/": {
-          target: "http://192.168.100.112:3000",
+          target: `http://${env.VITE_PRIVATE_IP}:3000`,
         },
         "/socket.io": {
-          target: "http://192.168.100.112:3000",
+          target: `http://${env.VITE_PRIVATE_IP}:3000`,
           changeOrigin: true,
           secure: true,
           ws: true,
